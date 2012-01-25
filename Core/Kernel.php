f4f66de347376a1e13c90a2bfe4cb920e16f2dfd
Hubert Perron <hubert.perron@gmail.com>
2012-01-24 22:03:18 -0500

    Added first version of the system kernel

---
+++ Core/Kernel.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Prototypr\SystemBundle\Core;
+
+/**
+ * Main Prototypr kernel
+ */
+class Kernel
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    private $request;
+
+    /**
+     * @var \Symfony\Bundle\DoctrineBundle\Registry
+     */
+    private $doctrine;
+
+    /**
+     * @var \Symfony\Bridge\Monolog\Logger
+     */
+    private $logger;
+
+    /**
+     * Init
+     */
+    public function init()
+    {
+        $this->logger->info(get_class($this) . '::INIT');
+    }
+
+    /**
+     * Set Request
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request The Request
+     */
+    public function setRequest($request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Set Doctrine
+     *
+     * @param \Symfony\Bundle\DoctrineBundle\Registry $doctrine Doctrine
+     */
+    public function setDoctrine($doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * Set Logger
+     *
+     * @param \Symfony\Bridge\Monolog\Logger $logger Monolog logger
+     */
+    public function setLogger($logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Get the current application name
+     *
+     * @return string|null
+     */
+    public function getCurrentApplicationName()
+    {
+        // Match each camel cased token
+        $controller = str_replace('\\', '', $this->request->get('_controller'));
+        $tokens = preg_split('/(?<=\\w)(?=[A-Z])/', $controller);
+
+        if (isset($tokens[1])) {
+            return strtolower($tokens[1]);
+        }
+    }
+}
