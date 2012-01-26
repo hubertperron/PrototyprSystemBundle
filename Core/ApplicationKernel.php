0f2f3a5a2d43b41a84e84d2b0f845867b20d242e
Hubert Perron <hubert.perron@gmail.com>
2012-01-25 21:46:50 -0500

    Adding base implementation and interface for the application kernels

---
+++ Core/ApplicationKernel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Prototypr\SystemBundle\Core;
+
+/**
+ * Default prototypr application kernel implementation
+ */
+class ApplicationKernel
+{
+    /**
+     * Init
+     */
+    public function init()
+    {
+        // ... add your implementation in the subclasses
+    }
+}
---
+++ Core/ApplicationKernelInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prototypr\SystemBundle\Core;
+
+/**
+ * Prototypr application kernel Interface
+ */
+interface ApplicationKernelInterface
+{
+    /**
+     * Initialize the controller.
+     *
+     * @abstract
+     */
+    function init();
+}
