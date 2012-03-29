29335a4a3c4bff30fec03348d2f7fe01aaac7689
hubertperron <hubert.perron@gmail.com>
2012-03-28 20:33:50 -0400

    Forgotten to add the Base entity abstract class

---
+++ Entity/Base.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Prototypr\SystemBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Prototypr\SystemBundle\Entity\Base
+ */
+abstract class Base
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var datetime $createdAt
+     */
+    private $createdAt;
+
+    /**
+     * @var datetime $updatedAt
+     */
+    private $updatedAt;
+
+    /**
+     * @var integer $ordering
+     */
+    private $ordering;
+
+    /**
+     * @var boolean $active
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return datetime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set ordering
+     *
+     * @param integer $ordering
+     */
+    public function setOrdering($ordering)
+    {
+        $this->ordering = $ordering;
+    }
+
+    /**
+     * Get ordering
+     *
+     * @return integer 
+     */
+    public function getOrdering()
+    {
+        return $this->ordering;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean 
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
