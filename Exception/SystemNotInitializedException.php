7fc0928bc5e8405dd4b2c27ab7e2c4ebe6d3f842
hubertperron <hubert.perron@gmail.com>
2012-04-13 20:15:29 -0400

    Fixing CS in SystemNotInitializedException

--- Exception/SystemNotInitializedException.php
+++ Exception/SystemNotInitializedException.php
@@ -14,8 +14,8 @@ class SystemNotInitializedException extends \Exception
      * @param int $code
      * @param \Exception $previous
      */
-    public function __construct($message = "", $code = 0, \Exception $previous = null) {
-
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
         $this->message = $message ?: 'Prototypr is not initialized. The controller class is not extending prototypr base controller or the matched route is missing some parameters. See <abbr title="Prototypr\SystemBundle\Listener">ControllerListener->isPrototyprEnabled()</abbr> for more details.';
     }
 }
